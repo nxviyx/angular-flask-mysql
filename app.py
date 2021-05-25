36ebb931afac455e0c88384271d49ce29c99ebf9
Naveen Fernando <malinda0610@gmail.com>
2021-05-25 07:08:00 +0530

    first commit

--- app.py
+++ app.py
@@ -1,16 +1,30 @@
-from flask import Flask,render_template, request, jsonify
-from flask_compress import Compress
+# from flask import Flask,render_template, request, jsonify
+# from flask_compress import Compress
 
-# initialize flask application
-app = Flask(__name__,static_url_path='',static_folder='dist',template_folder='dist')
-Compress(app)
+# # initialize flask application
+# app = Flask(__name__,static_url_path='',static_folder='dist',template_folder='dist')
+# Compress(app)
 
+
+# @app.route('/')
+# def test():
+#     return render_template("index.html")
+
+
+# if __name__ == '__main__':
+#     # run web server
+#     app.run()
+
+
+from flask import Flask
+
+app = Flask(__name__, static_url_path='', static_folder='dist')
 
 @app.route('/')
-def test():
-    return render_template("index.html")
+def index():
+    return app.send_static_file('index.html')
 
-
-if __name__ == '__main__':
-    # run web server
-    app.run()
+@app.route('/<path:path>')
+def static_proxy(path):
+    # send_static_file will guess the correct MIME type
+    return app.send_static_file(path)
--- procfile
+++ procfile
@@ -1,1 +1,1 @@
-web: gunicorn wsgi:app
+web: flask run --host=0.0.0.0 --port=$PORT
